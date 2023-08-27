7f9290702f05fc4871f6f762449e05b42ee2fdc4
Kishan Moliya <94744840+kishanmoliya@users.noreply.github.com>
2023-08-27 12:58:04 +0530

    Delete ASP.net directory

--- ASP.net/iPortfolio/iPortfolio/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace iPortfolio.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
